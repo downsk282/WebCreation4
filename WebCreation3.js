20538353da079f7d03761bdcd5c3a78ff76a5feb
downsk282 <70290576+downsk282@users.noreply.github.com>
2020-10-29 22:37:27 -0400

    Add files via upload

---
+++ WebCreation3.js
@@ -0,0 +1,8 @@
+function alertHover(){
+    document.getElementById("area").style.backgroundColor = "green";
+    alert("Click to reveal my favorite color.");
+  }
+  
+  function reset(){
+     document.getElementById("area").style.backgroundColor = "#e087c1"; 
+  }
